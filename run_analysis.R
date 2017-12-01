af240033a8b6ac166d101dde06a14980196bea24
DawidDabkowski <dav.dabkowski@gmail.com>
2017-12-01 16:38:25 +0100

    version 2 - trail

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,59 @@
-clean_data <- function(set){
-    features <- readLines(paste0(".\\", "UCI HAR Dataset\\", "features.txt"))
-    checkpoint1 <- length(unique(features))
-    features <- gsub(" ", "", features)
-    features <- gsub("-", "", features)
-    features <- gsub(",", "", features)
-    features <- gsub("\\(", "", features)
-    features <- gsub("\\)", "", features)
-    features <- tolower(features)
-    checkpoint2 <- length(unique(features))
-    if (checkpoint1!=checkpoint2)
-        warning("New variable names are not unique!")
-    
-    X <- read.fwf(paste0(".\\", "UCI HAR Dataset\\", set, "\\X_", set, ".txt"), widths=rep(16, 561))
-    colnames(X) <- features
+#POINT 1
+X_train <- read.fwf(".\\UCI HAR Dataset\\train\\X_train.txt", widths=rep(16, 561))
+X_test <- read.fwf(".\\UCI HAR Dataset\\test\\X_test.txt", widths=rep(16, 561))
+X <- rbind(X_train, X_test)
 
-    y <- read.fwf(paste0(".\\", "UCI HAR Dataset\\", set, "\\y_", set, ".txt"), 16)
-    y <- factor(unlist(y))
-    names(y) <- "activity"
-    
-    subject <- read.fwf(paste0(".\\", "UCI HAR Dataset\\", set, "\\subject_", set, ".txt"), 16)
-    subject <- factor(unlist(subject))
-    names(subject) <- "subject"
-    
-    tidy_data <- cbind(y, subject, X)
-    write.table(tidy_data, paste0(set, ".txt"), row.names=F)
-}
+features <- readLines(".\\UCI HAR Dataset\\features.txt")
+colnames(X) <- features
 
-clean_data("train")
-clean_data("test")
+y_train <- read.fwf(".\\UCI HAR Dataset\\train\\y_train.txt", 16)
+y_test <- read.fwf(".\\UCI HAR Dataset\\test\\y_test.txt", 16)
+y <- rbind(y_train, y_test)
+y <- factor(unlist(y))
 
-#activities <- readLines(paste0(".\\", "UCI HAR Dataset\\", "activity_labels.txt"))
+subject_train <- read.fwf(".\\UCI HAR Dataset\\train\\subject_train.txt", 16)
+subject_test <- read.fwf(".\\UCI HAR Dataset\\test\\subject_test.txt", 16)
+subject <- rbind(subject_train, subject_test)
+subject <- factor(unlist(subject))
+
+tidy_data <- cbind(y, subject, X)
+colnames(tidy_data)[1:2] <- c("activity", "subject")
+
+#POINT 2
+
+cols <- colnames(tidy_data)
+good_cols <- grep("mean\\(|std\\(", cols)
+tidy_data <- tidy_data[,c(1,2,good_cols)]
+
+#POINT 3
+
+activities <- readLines(".\\UCI HAR Dataset\\activity_labels.txt")
+activities <- substr(activities, 3, 100)
+activities <- gsub("_", "", activities)
+activities <- tolower(activities)
+levels(tidy_data$activity) <- activities
+
+#POINT 4
+cols <- colnames(tidy_data)
+checkpoint1 <- length(unique(cols))
+cols <- gsub(" ", "", cols)
+cols <- gsub("-", "", cols)
+cols <- gsub(",", "", cols)
+cols <- gsub("\\(", "", cols)
+cols <- gsub("\\)", "", cols)
+cols <- tolower(cols)
+checkpoint2 <- length(unique(cols))
+if (checkpoint1!=checkpoint2)
+    warning("New variable names are not unique!")
+colnames(tidy_data) <- cols
+
+write.table(tidy_data, "tidy_data.txt", row.names=F)
+
+#POINT 5
+library(dplyr)
+tidy_data_agg <- tidy_data %>% group_by(activity, subject) %>% summarize_all(funs(mean))
+
+write.table(tidy_data_agg, "tidy_data_agg.txt", row.names=F)
+
+
 #body_acc_x <- read.fwf(paste0(".\\", "UCI HAR Dataset\\", set, "\\", "Inertial Signals\\", "body_acc_x_", set, ".txt"), 16)
